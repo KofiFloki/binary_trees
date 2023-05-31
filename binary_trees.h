bde5cb6637f316ee12fb0640e80a475aa05be0c8
KofiFloki <Jeremiah.martey@yahoo.com>
2023-05-30 17:34:56 -0700

    Project Binary

--- binary_trees.h
+++ binary_trees.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -27,6 +26,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
